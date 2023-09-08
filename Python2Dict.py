bc2566b961e23d3471815c71dc329de6cd3438bb
AndrewTaro <36262823+AndrewTaro@users.noreply.github.com>
2023-09-08 21:18:34 +0900

    Fixed incorrect iteration order

--- Python2Dict.py
+++ Python2Dict.py
@@ -4,47 +4,79 @@ PERTURB_SHIFT = 5
 class Python2Dict(dict):
     def iterkeys(self):
         #https://github.com/python/cpython/blob/v2.7.12/Objects/dictobject.c#L364-L366
-        mask = self._get_mask()
-        hashdict = {}
-        for k in self:
-            hash_val = hash(k)
-            hash_idx = self._get_hashindex(hash_val, mask)
+        hashtable = {}
+        count = 0
+        while len(hashtable) < len(self):
+            size = self.__get_dksize(hashtable)
+            mask = size - 1
+            temptable = {}
+            used_keys = set()
 
-            if hash_idx in hashdict:
-                perturb = hash_val
-                while True:
-                    hash_idx = self._get_linearprobing(hash_idx, perturb, mask)
-                    if hash_idx not in hashdict:
-                        break
-                    perturb = perturb >> PERTURB_SHIFT
-        
-            hashdict[hash_idx] = k
+            for hashkey in sorted(hashtable): #preseiving the hash order before resizing
+                key = hashtable[hashkey]
+                hash_idx = self._get_hashindex(key, mask, temptable)
+                used_keys.add(key)
+                temptable[hash_idx] = key
 
-        return [hashdict[i] for i in sorted(hashdict)].__iter__()
+            for key in self: #preserving the order of insertion
+                if key in used_keys:
+                    continue
+                hash_idx = self._get_hashindex(key, mask, temptable)
+                temptable[hash_idx] = key
+                if len(temptable)*3 >= size*2: #need resize
+                    break
+            count += 1
+            hashtable = temptable
+
+        for hashkey in sorted(hashtable):
+            yield hashtable[hashkey]
     
     def itervalues(self):
-        return [self[i] for i in self.iterkeys()].__iter__()
+        for i in self.iterkeys():
+            yield self[i]
     
-    def _get_hashindex(self, hash, mask):
+    def iteritems(self):
+        for i in self.iterkeys():
+            yield (i, self[i])
+    
+    def _get_hashindex(self, key, mask, hashtable):
+        """
+        calculates index in the hashtable from key, current dict size and hashtable.
+        """
+        #https://github.com/python/cpython/blob/v2.7.12/Objects/dictobject.c#L364-L366
+        hash_val = hash(key)
+        hash_idx = self.__apply_mask(hash_val, mask)
+
+        if hash_idx in hashtable:
+            perturb = hash_val
+            while True:
+                hash_idx = self.__apply_linearprobing(hash_idx, perturb, mask)
+                if hash_idx not in hashtable:
+                    break
+                perturb = perturb >> PERTURB_SHIFT
+    
+        return hash_idx
+    
+    def __apply_mask(self, hash, mask):
         #https://github.com/python/cpython/blob/v2.7.12/Objects/dictobject.c#L331
         return hash & mask
     
-    def _get_linearprobing(self, hashindex, perturb, mask):
+    def __apply_linearprobing(self, hashindex, perturb, mask):
         #https://github.com/python/cpython/blob/v2.7.12/Objects/dictobject.c#L365
         return (hashindex * 5 + perturb + 1) & mask
     
-    def _get_dksize(self):
+    def __get_dksize(self, dict):
         #https://github.com/python/cpython/blob/v2.7.12/Objects/dictobject.c#L802-L819
-        length = len(self)
+        length = len(dict)
         size_coeff = 4
         count = 0
         if length > 50000:
             size_coeff = 2
         while True:
-            new_size = DK_SIZE * (size_coeff ** count)
-            if (length * 3) < (new_size * 2):
-                return new_size
+            size = DK_SIZE * (size_coeff ** count)
+            if (length * 3) < (size * 2):
+                return size
             count += 1
 
-    def _get_mask(self):
-        return self._get_dksize() - 1
+    def __get_mask(self, size):
+        return size - 1
